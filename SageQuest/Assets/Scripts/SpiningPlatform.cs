98bd434c7dc29dd5becc1ee722e3240df980008f
Chris Guinto <cjguinto12@gmail.com>
2019-05-29 20:31:04 -0700

    Stuff

---
+++ SageQuest/Assets/Scripts/SpiningPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiningPlatform : MonoBehaviour
+{
+    public float speed = 1f;
+    public bool rotateLeft = false;
+	
+    // Use this for initialization
+	void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RotateDirection();
+    }
+
+    void RotateDirection()
+    {
+        if (rotateLeft == false)
+        {
+            transform.Rotate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (rotateLeft == true)
+        {
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
