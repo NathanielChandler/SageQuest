0d3f3d02d9c44ba0ccd63853d08754c45470c1cd
Chris Guinto <cjguinto12@gmail.com>
2019-06-03 16:40:28 -0700

    Stuff

--- SageQuest/Assets/Scripts/PlayerController.cs
+++ SageQuest/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     public Transform groundCheck;
     public ContactPoint2D[] contactPoints;
     public ContactFilter2D contactFilter;
+    public int numberOfHerbs = 0;
 
     private bool grounded = false;
     private bool positiveXCollision = false;
@@ -164,8 +165,12 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("IsJumping", false);
         }
+        if (collision.gameObject.name.Contains("Herb"))
+        {
+            numberOfHerbs += 1;
+        }
 
-        Debug.Log(collision.gameObject.name.Contains("Moving Platform"));
+            Debug.Log(collision.gameObject.name.Contains("Moving Platform"));
     }
 
     private void OnCollisionExit2D(Collision2D collision)
